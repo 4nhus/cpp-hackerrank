a628a021d7a0c12fb88e7a5dec497c65d0dde4e9
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2023-11-09 15:50:04 +1100

    Add New Year Chaos solution

--- arrays.cpp
+++ arrays.cpp
@@ -48,3 +48,62 @@ vector<int> rotLeft(vector<int> a, int d) {
 
     return a;
 }
+
+int merge_and_count(vector<int> &q, int l, int r) {
+    if (l >= r) return 0;
+
+    int middle = (l + r) / 2;
+    int l_count = merge_and_count(q, l, middle);
+    int r_count = merge_and_count(q, middle + 1, r);
+    int merge_count = 0;
+
+    int l_index = l;
+    int r_index = middle + 1;
+    vector<int> qMerge;
+    while (l_index <= middle && r_index <= r) {
+        if (q[l_index] > q[r_index]) {
+            qMerge.push_back(q[r_index]);
+            r_index++;
+            merge_count += middle - l_index + 1;
+        } else {
+            qMerge.push_back(q[l_index]);
+            l_index++;
+        }
+    }
+
+    while (l_index <= middle) {
+        qMerge.push_back(q[l_index]);
+        l_index++;
+    }
+
+    while (r_index <= r) {
+        qMerge.push_back(q[r_index]);
+        r_index++;
+    }
+
+    int i = l;
+    for (int num: qMerge) {
+        q[i] = num;
+        i++;
+    }
+
+    return l_count + r_count + merge_count;
+}
+
+/*
+ * Complete the 'minimumBribes' function below.
+ *
+ * The function accepts INTEGER_ARRAY q as parameter.
+ */
+void minimumBribes(vector<int> q) {
+    for (int i = 0; i < q.size(); i++) {
+        int correct_sticker = i + 1;
+
+        if (q[i] - correct_sticker > 2) {
+            cout << "Too chaotic" << endl;
+            return;
+        }
+    }
+
+    cout << merge_and_count(q, 0, q.size() - 1) << endl;
+}
